28d23edebf45e6849f7ef21085fad447ddc330e7
ST10448109 <ST10448109@vcconnect.edu.za>
2025-05-19 17:28:35 +0200

    second commit
    - gui complete

--- app/src/main/java/vcmsa/ci/quizapp/QuizQuestions.kt
+++ app/src/main/java/vcmsa/ci/quizapp/QuizQuestions.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import vcmsa.ci.quizapplication.R
 
 class QuizQuestions : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
